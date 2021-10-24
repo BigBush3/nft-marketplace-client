95089b5cded1e807adea645c8ef304abd2fbfd74
Andrey <andrey.platonov2005@gmail.com>
2021-10-24 12:57:18 +0300

    sth

--- components/global/CreateForm.tsx
+++ components/global/CreateForm.tsx
@@ -332,7 +332,7 @@ const approved = await NFT.methods.isApprovedForAll(walletAddress, NFTSTORE_ADDR
                 console.log(error);
                 console.log(res);
               
-                const result = await axios.post('https://nft-marketplace-api-plzqa.ondigitalocean.app/nft/subscription', {id: data._id, userId: cookie.get('id'), contractAddress: TIMEDAUCTION_ADDRESS, topic: EVENTS_TOPICS.Time_Auction_Created, tokenId: something, type: 'timedAuction', status: 'active' })
+                const result = await axios.post('https://nft-marketplace-api-plzqa.ondigitalocean.app/nft/subscription', {id: resp.data.resClient, userId: cookie.get('id'), contractAddress: TIMEDAUCTION_ADDRESS, topic: EVENTS_TOPICS.Time_Auction_Created, tokenId: something, type: 'timedAuction', status: 'active' })
                 router.push(`/product/${result.data.resClient._id}`)
             }
         )		
@@ -354,7 +354,7 @@ const approved = await NFT.methods.isApprovedForAll(walletAddress, NFTSTORE_ADDR
 		        console.log(error);
 		        console.log(res);
             console.log(resp.data.resClient._id)
-            const result = await axios.post('http://localhost:8000/nft/subscription', {id: resp.data.resClient._id, userId: cookie.get('id'), contractAddress: SIMPLEAUCTION_ADDRESS, topic: EVENTS_TOPICS.FIX_ORDER_CREATED, tokenId: something, status: 'active', type: 'orderSell'})
+            const result = await axios.post('https://nft-marketplace-api-plzqa.ondigitalocean.app/nft/subscription', {id: resp.data.resClient._id, userId: cookie.get('id'), contractAddress: SIMPLEAUCTION_ADDRESS, topic: EVENTS_TOPICS.FIX_ORDER_CREATED, tokenId: something, status: 'active', type: 'orderSell'})
             console.log(result.data)
             router.push(`/product/${result.data.resClient._id}`)
 		    }
